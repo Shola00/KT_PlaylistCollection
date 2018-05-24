dfbf935a4f4ba8ce18b8ed16bfdbb664b52125c7
Shola <sholaemmanuel00@gmail.com>
2018-05-24 14:34:38 +0100

    starting a new project implementing oop

---
+++ PlaylistCollection/PlaylistCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlaylistCollection
+//
+//  Created by Shola Emmanuel on 24/05/2018.
+//  Copyright © 2018 KaiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
